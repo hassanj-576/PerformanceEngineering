2dc72a8d4d6e99f431fb825c94f53371c5095dfd
Hassan <hassan@Hassans-MacBook-Pro.local>
2017-04-10 22:22:45 +0200

    Added a python script to generate data

---
+++ Assignment1/datagenerator.py
@@ -0,0 +1,17 @@
+import random
+import time
+import sys
+temp=""
+M=int(sys.argv[2])
+N=int(sys.argv[3])
+random.seed( time.time() )
+f = open(sys.argv[1], 'w')
+for row in xrange(0,M):
+	temp=""
+	for column in xrange(0,N):
+		temp=temp+str(random.uniform(0,100))+" "
+	f.write(temp+"\n") 
+	
+
+
+f.close()
